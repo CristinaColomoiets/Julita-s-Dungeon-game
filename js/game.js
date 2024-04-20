336146e5731965808360d74b4f12636dc0a0c3a9
Paloma Rehecho <paloma_re@hotmail.com>
2024-04-20 22:28:07 +0100

    starting player

--- js/game.js
+++ js/game.js
@@ -1,4 +1,5 @@
 const Game = {
+
     gameScreen: document.querySelector('#game-screen'),
     gameSize: {
         w: window.innerWidth,
@@ -7,7 +8,7 @@ const Game = {
 
     //background: undefined,
     //speedPlayerCounter: 0,
-    //player: undefined,
+    player: undefined,
     //gamePath: undefined,
     stepSquare: undefined,
 
@@ -16,7 +17,7 @@ const Game = {
         this.createElement();
         console.log('La clase Game funciona!')
     },
-    
+
     setGameScreenSize() {
         this.gameScreen.style.width = `${this.gameSize.w}px`   //css properties to take into account
         this.gameScreen.style.height = `${this.gameSize.h}px`
@@ -24,17 +25,20 @@ const Game = {
 
     createElement() {
         const stepSquare1 = new StepSquare(this.gameSize, 350, 250)
-        
-        const stepSquarePosLeft  = stepSquare1.stepSquarePos.left
+
+        const stepSquarePosLeft = stepSquare1.stepSquarePos.left
         const stepSquareHeight = stepSquare1.stepSquareSize.h
         const stepSquareWidth = stepSquare1.stepSquareSize.w
 
-        const stepSquare2 = new StepSquare(this.gameSize, stepSquarePosLeft, stepSquare1.stepSquarePos.top - stepSquareHeight - 1, 'zombie');
+        const stepSquare2 = new StepSquare(this.gameSize, stepSquarePosLeft, stepSquare1.stepSquarePos.top - stepSquareHeight - 1, 'zombie')
         const stepSquare3 = new StepSquare(this.gameSize, stepSquarePosLeft, stepSquare2.stepSquarePos.top - stepSquareHeight - 1)
         const stepSquare4 = new StepSquare(this.gameSize, stepSquarePosLeft, stepSquare3.stepSquarePos.top - stepSquareHeight - 1)
         const stepSquare5 = new StepSquare(this.gameSize, stepSquarePosLeft + stepSquareWidth + 1, stepSquare4.stepSquarePos.top)
 
-            
+        const player = new Player(this.gameSize, this.gameScreen, 200, 200)
+
+
+
         // this.background = new Background(this.gameScreen, this.gameSize)
         //this.gamePath = []
     }
--- js/player.js
+++ js/player.js
@@ -1,17 +1,37 @@
 class Player {
 
-    constructor(gameSize) {
+    constructor(gameSize, gameScreen, leftValue, topValue) {
 
         this.gameSize = gameSize
+        this.gameScreen = gameScreen
 
         this.playerSize = {
             w: 65,
-            h: 65, 
+            h: 65,
         }
 
         this.playerPos = {
             left: leftValue,
             top: topValue
         }
+
+        this.init()
+
+    }
+
+
+    init() {
+        this.playerOne = document.createElement('div');
+        this.playerOne.style.position = "absolute"
+        this.playerOne.style.backgroundColor = 'pink'
+
+        this.playerOne.style.width = `${this.playerSize.w}px`
+        this.playerOne.style.height = `${this.playerSize.h}px`
+        this.playerOne.style.left = `${this.playerPos.left}px`
+        this.playerOne.style.top = `${this.playerPos.top}px`
+    }
+
+    move() {
+
     }
 }
